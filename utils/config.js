6456f829d3ff37f434b43481f2654c654c593af2
Dmitriy Bachromov <dvbak@yandex.ru>
2023-11-10 11:54:42 +0300

    edit address port

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const {
-  PORT = 4000,
+  PORT = 3000,
   DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
   NODE_ENV = 'development',
   SECRET_KEY = 'secret signature key',
